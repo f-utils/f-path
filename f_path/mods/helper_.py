ae58415440e7998fad314052c0a9794c050116ac
yxm <yxm.dev@gmx.ie>
2025-02-20 08:11:32 -0300

    feat: add basic functions

--- f_path/mods/helper_.py
+++ f_path/mods/helper_.py
@@ -8,3 +8,8 @@ def  is_path(x):
         except Exception:
             return False
     return isinstance(x, Pathlib)
+
+def check_path(*args):
+    for x in args:
+        if not is_path(x):
+            raise AttributeError(f"'{x}' is not a path.")
