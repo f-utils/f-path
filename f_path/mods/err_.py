e8079aba11da05bcfeb4eab77fc88ad5b2c164ee
yxm <yxm.dev@gmx.ie>
2025-02-20 09:44:34 -0300

    feat: add error classes

---
+++ f_path/mods/err_.py
@@ -0,0 +1,29 @@
+class IsNotPath(Exception):
+    pass
+
+class IsNotDir(Exception):
+    pass
+
+class IsNotFile(Exception):
+    pass
+
+class IsNotSymlink(Exception):
+    pass
+
+class IsNotMount(Exception):
+    pass
+
+class IsNotPosix(Exception):
+    pass
+
+class IsNotWindows(Exception):
+    pass
+
+class IsNotRelative(Exception):
+    pass
+
+class IsNotAbsolute(Exception):
+    pass
+
+class NotExist(Exception):
+    pass
